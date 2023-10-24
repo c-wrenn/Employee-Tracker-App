fdef75842440d54501957d5be2f8d9becadf8a56
c-wrenn <crislynwren@yahoo.com>
2023-10-24 15:56:49 -0500

    "Schema complete, fixing seeds"

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,27 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
-
 CREATE DATABASE employee_tracker_db;
 
 -- use this database so that the following code will affect this database 
 USE employee_tracker_db;
 
--- will hold names of each employee 
-CREATE TABLE employees (
-    --autoincrements when a new employee is added
-    id INT  NOT NULL,
-  firstName VARCHAR(30),
-  lastName VARCHAR(30),
-  department NOT NULL,
-  empRole NOT NULL
-);
--- holds department job title --
---CREATE TABLE department (
-  -- id INT PRIMARY KEY
-  -- departmentTitle VARCHAR(30)
---);
--- holds each employees salary --
---CREATE TABLE roles (
-  --  id INT PRIMARY KEY,
-  --  department NOT NULL,
-  --  salary DECIMAL
 
---);
+CREATE TABLE employee (
+    
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  firstName VARCHAR(30) NOT NULL,
+  lastName VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT 
+);
+
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  department_id INT,
+  salary DECIMAL
+);
+
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  depName VARCHAR(30)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,32 @@
---Populate our databases with data here--
-INSERT INTO  employees (id, firstName, lastName, empRole)
-VALUES
-(1, Alice, Thorton, Finance),
-(2, Ivy, Mathis, Accounting),
-(3, Crislyn, Wren , Computer Engineer),
-(4, Sally, Doe, Finance),
-(5,Beckett, Harvey, Sales Representative);
 
---INSERT INTO roles (id, departmentTitle, salary)
---VALUES
---(1, Finance),
---()
+INSERT INTO  employee (id, firstName, lastName, role_id, manager_id)
+VALUES
+(1, "Alice", "Thorton",(SELECT id FROM role WHERE title = 'Accounting'),
+(SELECT firstName FROM employee WHERE role_id = 'Manager' )),
+(2, "Ivy", "Mathis", (SELECT id FROM role WHERE title = 'Sales Lead'),
+(SELECT firstName FROM employee WHERE role_id = 'Manager' )),
+(3, "Crislyn", "Wren" , (SELECT id FROM role WHERE title = 'Computer_Engineer'),
+ (SELECT firstName FROM employee WHERE role_id = 'Manager' )),
+(4, "Sally", "Doe", (SELECT id FROM role WHERE title = 'Accounting'),
+(SELECT firstName FROM employee WHERE role_id = 'Manager' )),
+(5, "Beckett", "Harvey", (SELECT id FROM role WHERE title = 'Lawyer'),
+ (SELECT firstName FROM employee WHERE role_id = 'Manager' )),
+(6, "Denesha", "Luke", (SELECT id FROM role WHERE title = 'Manager'),
+('NULL' ));
+
+
+INSERT INTO role (id, title, department_id, salary)
+VALUES
+(1, "Accounting", (SELECT id FROM department WHERE depName = 'Finance' ), 300000),
+(2, "Sales Lead", (SELECT id FROM department WHERE depName = 'Sales' ), 150000),
+(3, "Computer_Engineer", (SELECT id FROM department WHERE depName = 'Technology' ),250000),
+(4, "Lawyer",(SELECT id FROM department WHERE depName = 'Legal' ), 475000),
+(5, "Manager", (SELECT id FROM department WHERE depName = 'Management'), 135000);
+
+INSERT INTO department (id, depName)
+VALUES
+(1, 'Finance'),
+(2, 'Sales'),
+(3, 'Technology'),
+(4, 'Legal'),
+(5, 'Management');
